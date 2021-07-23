7e8538f75a11ccc91da4d1724e814ed7ca5308a9
rohitnohwar <rohitnohwar@DESKTOP-63SV5DP>
2021-07-24 02:55:13 +0530

    j

--- server.js
+++ server.js
@@ -199,6 +199,10 @@ const port = process.env.PORT || 3001;
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static("client/build"));
+
+    app.get("*", function(req, res){
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 app.listen(port, () => {
