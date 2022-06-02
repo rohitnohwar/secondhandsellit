a78dc214b2095a218106f66d9ee8c4f0c8eb054c
rohit <rohit.nohwar@gmail.com>
2022-06-02 16:04:37 +0530

    jhnaq

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import Home from "./Home";
-import Profile from "./Profile";
-import Register from "./Register"
-import Login from "./Login";
+import React, { Suspense } from 'react';
+// import Home from "./Home";
+// import Profile from "./Profile";
+// import Register from "./Register"
+// import Login from "./Login";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+const Home = React.lazy(() => import('./Home.js'));
+const Profile = React.lazy(() => import('./Profile.js'));
+const Register = React.lazy(() => import('./Register.js'));
+const Login = React.lazy(() => import('./Login.js'));
+
+
+// function App(){ 
+
+//   return <Router>
+//   <div>
+//   <Switch>
+//     <Route path="/" exact component={Login} />
+//     <Route path="/register" exact component={Register} />
+//     <Route path="/profile" exact component={Profile} />
+//     <Route path="/home" exact component={Home} />
+//     <Route path="/home/:c" exact component={Home} />
+//   </Switch>
+//   </div>
+//   </Router>
+// }
+
 
 function App(){ 
 
   return <Router>
   <div>
+  <Suspense fallback={<div style={{margin:'auto', textAlign:'center',fontSize:'30px', fontWeight:'800'}}>Loading...</div>}>
   <Switch>
     <Route path="/" exact component={Login} />
     <Route path="/register" exact component={Register} />
@@ -16,6 +39,7 @@ function App(){
     <Route path="/home" exact component={Home} />
     <Route path="/home/:c" exact component={Home} />
   </Switch>
+  </Suspense>
   </div>
   </Router>
 }
