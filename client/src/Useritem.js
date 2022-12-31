23b782242db3127d3c1e09d6913c4c83f66f9ef8
rohit nohwar <rohit.nohwar@gmail.com>
2022-12-31 16:55:21 +0530

    dee

--- client/src/Useritem.js
+++ client/src/Useritem.js
@@ -21,7 +21,7 @@ function Useritem(props){
 
     return <div class="item">
     <div class="s"></div>
-    <div class="img-div"><img src={"https://imagesforsecondhandsellit.s3.ap-southeast-1.amazonaws.com/"+props.image} alt="none" class="img-class"></img></div>
+    <div class="img-div"><img src={"https://secondhandsellit.s3.ap-south-1.amazonaws.com/"+props.image} alt="none" class="img-class"></img></div>
     <div>Item:-{props.item}</div>
     <div>Name:-{props.name}</div>
     <div>Phone number:-{props.number}</div>
